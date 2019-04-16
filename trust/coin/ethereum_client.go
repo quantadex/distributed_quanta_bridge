6f80d54dccd5722e12f5d55388726d3f7db9151f
Quoc <quocble@gmail.com>
2019-04-16 15:58:56 -0700

    fix build for new change with ethereum

--- trust/coin/ethereum_client.go
+++ trust/coin/ethereum_client.go
@@ -135,7 +135,7 @@ func (l *Listener) GetBlock(blockNumber int64) (*types.Block, error) {
 
 func (l *Listener) processBlock(block *types.Block) error {
 	transactions := block.Transactions()
-	blockTime := time.Unix(block.Time().Int64(), 0)
+	blockTime := time.Unix(int64(block.Time()), 0)
 
 	localLog := l.log.WithFields(log.F{
 		"blockNumber":  block.NumberU64(),
