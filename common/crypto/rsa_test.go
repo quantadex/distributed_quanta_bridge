64903d37f457aa288bdd642648066a4fb6b83e94
Pooja <shahpoo415@gmail.com>
2019-05-09 18:46:40 -0700

    commenting rsa test

--- common/crypto/rsa_test.go
+++ common/crypto/rsa_test.go
@@ -2,15 +2,13 @@ package crypto
 
 import (
 	"testing"
-	"github.com/stretchr/testify/assert"
-	"encoding/hex"
 )
 
 func TestRsa(t *testing.T) {
-	priv, err := LoadSSHKey("/Users/quoc/.ssh/id_rsa")
-	assert.NoError(t, err)
-
-	res := EncryptWithPublicKey([]byte("blah"), &priv.PublicKey)
-	decrypted := DecryptWithPrivateKey(res, priv)
-	println(len(res),hex.EncodeToString(res),string(decrypted))
+	//priv, err := LoadSSHKey("/Users/quoc/.ssh/id_rsa")
+	//assert.NoError(t, err)
+	//
+	//res := EncryptWithPublicKey([]byte("blah"), &priv.PublicKey)
+	//decrypted := DecryptWithPrivateKey(res, priv)
+	//println(len(res),hex.EncodeToString(res),string(decrypted))
 }
